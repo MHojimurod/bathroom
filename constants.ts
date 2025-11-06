f7b1ab204ebe8ebaea3da12fb7d5a15803262e50
MHojimurod <cool.hojimurod2001@gmail.com>
2025-11-06 23:40:09 +0500

    fix: endpoint

--- constants.ts
+++ constants.ts
@@ -3,7 +3,8 @@ import type { HeroSlide } from "./types";
 export const TELEGRAM_BOT_TOKEN =
   "7755094571:AAEezeM41_YaLdYIPubxruqan6Fu5DG62Y0";
 export const TELEGRAM_CHAT_ID = "-1002832678850";
-export const ENDPOINT = 'http://localhost:5001'
+export const ENDPOINT = 'https://api.casabella.uz'
+// export const ENDPOINT = 'http://localhost:5001'
 
 export const PRODUCTS_HERO_SLIDES: HeroSlide[] = [
   {
